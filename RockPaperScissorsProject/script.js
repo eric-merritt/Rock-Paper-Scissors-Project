1a4fed3376f227a2e7e83424353dba7db3ec66ec
eric-merritt <ericm2009@gmail.com>
2023-06-19 03:17:42 -0600

    Removed HTML elements, stuck with console design.

--- RockPaperScissorsProject/script.js
+++ RockPaperScissorsProject/script.js
@@ -1,79 +1,100 @@
-function game () {
+function game(){
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
 
-    const computerOptions = ['rock','paper','scissors'];
-    const computerSelection = getComputerChoice();
-
-function getComputerChoice() {
-    let randomChoice = (Math.floor(Math.random(computerOptions) * 3)); 
-    
-    if (randomChoice === 0) {
-        return "rock";
-    }   if (randomChoice === 1) {
-        return "paper";
-    }   if (randomChoice === 2) {
-        return "scissors";
+    function getComputerChoice(){
+        let computerOptions = ["rock", "paper", "scissors"];
+        let randomNumber = Math.floor(Math.random() * computerOptions.length);
+            if (randomNumber == 0){
+                return "rock";
+            } else if (randomNumber == 1){
+                return "paper";
+            } else if (randomNumber == 2){
+                return "scissors";
+            }       
     }
-}
 
-    const playerSelection = document.getElementsByClassName(buttongroup);
-
-    playerSelection.addEventListener("click", (event) => {
-        const isButton = event.target.nodeName === "BUTTON";
-        if (!isButton) {
-            return;
+    function playRound(playerSelection,computerSelection){
+        if (playerSelection.toLowerCase() == computerSelection){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "It's a tie!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            computerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You lose! Paper beats rock!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            playerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You win! Rock beats scissors!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            computerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You lose! Scissors beats paper!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            playerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You win! Paper beats rock!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            computerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You lose! Rock beats scissors!\n" + `${scoreboard}`;
+        } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"){
+            moves++;
+            movesLeft = `${5 - moves}`;
+            playerScore++;
+            scoreboard = "Player: " + `${playerScore}` + " Computer: " + `${computerScore}` + "\nMoves Left: " + `${movesLeft}`; 
+            return "You win! Scissors beats paper!\n" + `${scoreboard}`;
         }
-    })
-    
-    
-function playRound(playerSelection,computerSelection) { 
-           
-        const playerScoreBoard = document.querySelector(".p-count");
-        const computerScoreBoard = document.querySelector(".c-count");
-
-       if (playerSelection === 'rock') {
-       if (computerSelection === 'rock')  {
-        return "It's a tie!!"; 
-    
-    } else if (computerSelection === 'paper') {
-        return "The computer wins.";
-        computerScore++;
-        computerScoreBoard.textContent = computerScore;
-    } else  {
-        return "You win this round!!";
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;}
-
-      if (playerSelection === "paper") {
-      if (computerSelection === "paper") {
-         return "It's a tie!!";
-    }  else if (computerSelection === "scissors") {
-         return "The computer wins.";
-         computerScore++;
-         computerScoreBoard.textContent = computerScore;
-    } else {
-        return "You win this round!!";}
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-     if (playerSelection === "scissors") {
-     if (computerSelection === "rock") {
-        return "The computer wins.";
-        computerScore++;
-        computerScoreBoard.textContent = computerScore;
-    } else if (computerSelection === "paper") {
-        return "You win this round!!";
-    } else {
-        return "It's a tie!!";
     }
+
+for (i=1; i<=5; i++){   
+const playerSelection = prompt("Make a move! Rock/Paper/Scissors");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
+    if (moves == 5){
+        if (playerScore > computerScore){
+            var winner = "Player"
+        } else if (computerScore > playerScore){
+            var winner = "Computer";
+        }
+            alert("Game over! " + `${winner}` + " wins!");
+        }
 }
-    }
-}
-playRound(); 
+
 }
 
 game();
 
-}
+
+
+    
+    
+
+
+    
+
+                            
+
+
+
+
+
+
+
+
+
+
     
